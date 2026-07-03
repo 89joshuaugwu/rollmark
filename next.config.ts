c40707cfc5618c1bd511e76ca104c88655350670
Joshua Ugwu <joshuaugwu89@gmail.com>
2026-07-04 00:40:43 +0100

    fix: Configure Next.js webpack to handle ESM/CommonJS interop for jwks-rsa and jose

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: ["jwks-rsa", "jose"],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push({
+        "jwks-rsa": "commonjs jwks-rsa",
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
