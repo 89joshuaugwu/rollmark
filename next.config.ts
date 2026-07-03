a0538371211d1275aecee078ad662ca21874e0a5
Joshua Ugwu <joshuaugwu89@gmail.com>
2026-07-04 00:43:19 +0100

    fix: Update next.config.ts for Next.js 16 Turbopack - use serverExternalPackages instead of webpack

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ["jwks-rsa", "jose"],
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push({
-        "jwks-rsa": "commonjs jwks-rsa",
-      });
-    }
-    return config;
-  },
+  serverExternalPackages: ["jwks-rsa", "jose"],
+  turbopack: {},
 };
 
 export default nextConfig;
